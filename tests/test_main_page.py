55c4583a93e625c16d94e602a9e5cc1afa9fde32
DatenshiRay <112545089+DatenshiRay@users.noreply.github.com>
2025-06-19 23:44:12 +0500

    Add test for main page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,13 +1,13 @@
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     # Открываем главную страницу сайта
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-
+    main_page = MainPage(browser=browser, url=link)
+    main_page.open()
     # Находим и кликаем на ссылку для входа
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+ 
+    main_page.go_to_login_page()
 
     # Проверяем, что переход на страницу входа произошел успешно
     assert "login" in browser.current_url, "Login link is not working correctly"
