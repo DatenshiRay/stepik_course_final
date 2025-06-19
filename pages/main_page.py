903c85c954c5d4a0f8cfd702240693cb2623cef9
DatenshiRay <112545089+DatenshiRay@users.noreply.github.com>
2025-06-20 01:26:51 +0500

    All remaining files have been added, the project is now ready

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,10 +5,3 @@ from .base_page import BasePage
 class MainPage(BasePage):
     def __init__(self, browser, url):
         super().__init__(browser, url)
-
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
-        
-    def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
